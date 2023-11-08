45f34f7f32cf41d1571458c4c8212dc1366ad184
Eitan Worcel <96389636+mobbeitan@users.noreply.github.com>
2023-11-08 11:43:30 -0500

    Update xss-example.js

--- xss-example.js
+++ xss-example.js
@@ -2,6 +2,4 @@ var urlParams = new URLSearchParams(window.location.search);
 var name = urlParams.get('name');
 
 var unsafe_div = window.document.getElementById("vulnerable-div");
-//The below is vulnerable to XSS
-//unsafe_div.innerHTML = "Hello " + name; 
-unsafe_div.innerText = "Hello " + name;
+unsafe_div.innerHTML = "Hello " + name; 
