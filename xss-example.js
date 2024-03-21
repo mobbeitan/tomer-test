ed1f30577ac5c0a4a247110767539903ffd8f1e6
Eitan Worcel <96389636+mobbeitan@users.noreply.github.com>
2024-03-21 08:19:28 -0400

    Update xss-example.js

--- xss-example.js
+++ xss-example.js
@@ -3,3 +3,17 @@ var name = urlParams.get('name');
 
 var unsafe_div = window.document.getElementById("vulnerable-div");
 unsafe_div.innerHTML = "Hello " + name; 
+
+function findWhat () {
+  return (req: Request, res: Response, next: AnotherFunction) => {
+    const id = req.params.accountName
+
+    db.reviews.find({ $where: 'this.element == ' + id }).then((what: NextOne[]) => {
+      res.json(utils.queryResultToJson(what))
+    }, () => {
+      res.status(400).json({ error: 'Wrong Params' })
+    })
+  }
+}
+
+findWhat();
